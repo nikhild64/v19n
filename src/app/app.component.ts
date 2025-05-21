f53dcaef41ccbab8f5bef9d668c143f1893012de
Nikhil Dhawan <nikhil.dhawan@nagarro.com>
2025-05-21 16:24:21 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, linkedSignal, Signal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'v19n';
+  shippingOptions = signal([
+    'Standard Shipping',
+    'Two-Day Shipping',
+    'Overnight Shipping',
+  ]);
+
+  // Select the first shipping option by default.
+  selectedOption = linkedSignal(() => this.shippingOptions()[0]);
+
+  changeShipping(newOptionIndex: number) {
+    this.selectedOption.set(this.shippingOptions()[newOptionIndex]);
+  }
+
+  changeMethods() {
+    this.shippingOptions.set([
+      'ne Standard Shipping',
+      'ne Two-Day Shipping',
+      'ne Overnight Shipping',
+    ]);
+  }
 }
